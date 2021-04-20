b572be7b1afa3258e38f7afff945efea2b705b48
cennguyen <annp62923@gmail.com>
2021-04-20 15:12:00 +0700

    udpate login api

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -2,6 +2,9 @@ import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
 import User from "../models/userModel.js";
+import brcypt from "bcryptjs";
+import { generateToken } from "../utils.js";
+
 const userRouter = express.Router();
 
 userRouter.get(
@@ -12,4 +15,24 @@ userRouter.get(
     res.send({ createdUser });
   })
 );
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (brcypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
 export default userRouter;
